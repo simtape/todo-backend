551c8a98d2c781835f4c6a2596ca6ce47c56ba48
Simone Cassetta <simone.cassetta@datasound.it>
2024-01-20 17:36:10 +0100

    Initial commit

---
+++ src/test/java/it/unimol/todo/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimol.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
